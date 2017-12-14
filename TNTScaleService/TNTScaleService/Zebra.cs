c55dd0dcfa6ff5eb9ed5a1c21824c85b6e7bef7d
Jeremy Bond <jebond@trollandtoad.com>
2017-12-13 23:41:43 -0500

    asdfasdfsadf
    asdfasdfdsf

---
+++ TNTScaleService/TNTScaleService/Zebra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.SharpZebra;
+using Com.SharpZebra.Printing;
+
+namespace TNTScaleService
+{
+    class Zebra
+    {
+        public bool ConnectToPrinter()
+        {
+            PrinterSettings p = new PrinterSettings();
+            p.PrinterName = "ZTC ZP 450-200dpi";
+            USBPrinter printer = new USBPrinter(p);
+            byte[] woop = { 0x20, 0x20, 0x20 };
+            printer.Print(woop);
+            return true;
+        }
+    }
+}
